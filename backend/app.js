d7c9bc03cb0621917ad072142e8d984177be9ec4
ritvikraj115 <143361673+ritvikraj115@users.noreply.github.com>
2024-09-20 11:39:40 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL
+    origin: process.env.REACT_APP_FRONTEND_URL, // Replace with your frontend URL
     methods: ['GET', 'POST','PUT'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
